bb509a8d5cd36950920e89f90c38ea44ca5cf660
GoHo-DohJi <3dzhio7@gmail.com>
2026-02-07 22:47:18 +0200

    Add files via upload

---
+++ res/win32/resource.h
@@ -0,0 +1,13 @@
+#ifndef RESOURCE_H
+#define RESOURCE_H
+
+// Manifest resource ID (standard for Windows executables)
+#define IDR_MANIFEST 1
+
+// Version information
+#define VER_MAJOR       1
+#define VER_MINOR       0
+#define VER_PATCH       0
+#define VER_BUILD       0
+
+#endif // RESOURCE_H
